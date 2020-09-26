4a77a2a91eaf8998ddd50f4bfcf09cfb6f94f9fd
Vishal Devireddy <TwoTau@users.noreply.github.com>
2020-09-25 21:57:44 -0700

    Add short answer component

---
+++ frontend/src/student/QuizShortAnswer.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+/**
+ * Props:
+ * - placeholder: string
+ * - onClick: function
+ */
+class QuizShortAnswer extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	onSubmit = (event) => {
+		const answer = document.querySelector(".quiz-short-answer input").value;
+		this.props.onClick(answer);
+	}
+
+	render() {
+		return (
+			<div className="quiz-short-answer">
+				<input type="text" placeholder={this.props.placeholder} />
+				<button onClick={this.onSubmit}>Submit</button>
+			</div>
+		);
+	}
+}
+
+export default QuizShortAnswer;
