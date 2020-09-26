3e1f00c2ad8a06c40ab62734937cf95e227211a3
Jeremy Gunawan <sirknightj@gmail.com>
2020-09-25 19:40:19 -0700

    Added basic question structure to the student component

--- frontend/src/student/Student.js
+++ frontend/src/student/Student.js
@@ -5,8 +5,37 @@ class Student extends Component {
 		return (
 			<div className="student">
 				<h1>Student component</h1>
+				<QuizQuestion />
 			</div>
 		);
+	}
+}
+
+class QuizQuestion extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			prompt: "What is 1 + 1?",
+			answers: ["1", "2", "3", "4"],
+		}
+	}
+
+	render() {
+		const { prompt, answers } = this.state;
+		return (
+			<div className="quiz-question">
+				<div className="prompt">
+					{prompt}
+				</div>
+				<div className="answer-container">
+					{answers.map(answer => (
+						<p id={answer.id}>
+							{answer}
+						</p>
+					))}
+				</div>
+			</div>
+		)
 	}
 }
 
