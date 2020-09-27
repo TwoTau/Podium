d0068f8ec845e4b928d025a5c6b035afe791c4e5
Jeremy Gunawan <sirknightj@gmail.com>
2020-09-27 02:19:46 -0700

    Added hasSubmitted state for only 1 submission per student

--- frontend/src/student/QuizShortAnswer.js
+++ frontend/src/student/QuizShortAnswer.js
@@ -8,18 +8,23 @@ import React, { Component } from "react";
 class QuizShortAnswer extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			hasSubmitted: false,
+		}
 	}
 
 	onSubmit = (event) => {
 		const answer = document.querySelector(".quiz-short-answer input").value;
+		this.state.hasSubmitted = true;
 		this.props.onClick(answer);
 	}
 
 	render() {
 		return (
 			<div className="quiz-short-answer">
-				<input type="text" placeholder={this.props.placeholder || 'no-placeholder'} />
-				<button onClick={this.onSubmit}>Submit</button>
+				<input disabled={this.state.hasSubmitted} ref={input => this.answerInput = input} type="text" placeholder={this.props.placeholder || 'no-placeholder'} />
+				<button disabled={this.state.hasSubmitted} ref={button => this.submitButton = button} onClick={this.onSubmit}>Submit</button>
 			</div>
 		);
 	}
