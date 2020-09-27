b97a758869b81d2174ac78171bebd60fa025865e
Vishal Devireddy <TwoTau@users.noreply.github.com>
2020-09-27 00:03:36 -0700

    Start making UI nicer

--- frontend/src/teacher/StudentList.js
+++ frontend/src/teacher/StudentList.js
@@ -12,15 +12,12 @@ class StudentList extends Component {
     render() {
         console.log(this.props.students)
         return (
-            <div className="information-text">
-                <h1>StudentList</h1>
-                <ul>
-                    {this.props.students.sort((a, b) => {
-                        return b.score - a.score;
-                    }).map((student) =>
-                        <li key={student.username}>{student.username} ({student.score})</li>
-                    )}
-                </ul>
+            <div className="information-text student-list">
+                {this.props.students.sort((a, b) => {
+                    return b.score - a.score;
+                }).map((student) =>
+                    <div key={student.username}><span className="username">{student.username}</span><span className="score">{student.score}</span></div>
+                )}
             </div>
         );
     }
